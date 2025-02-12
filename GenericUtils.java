71de9f8f0ef318c8aac6db427dd0c1b197f8ed88
mkovacevic10 <max.billroth@gmail.com>
2025-02-12 23:57:53 +0100

    Methods added and main method filled for testing

--- GenericUtils.java
+++ GenericUtils.java
@@ -1,6 +1,37 @@
 /**
  * A utility class that provides generic methods for arrays.
+ * @author Maksym Kovacevic
+ * @version 12-01-2025
  */
 public class GenericUtils {
-
+    /**
+     * Reverses the elements of the given array in-place.
+     * @param array The array to be reversed.
+     * @param <T> The type of elements in the array.
+     */
+    public static <T> void reverse(T[] array) {
+        int left = 0;
+        int right = array.length - 1;
+        while (left < right) {
+            T temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+            left++;
+            right--;
+        }
+    }
+    /**
+     * Calculates the sum of all elements in the given array.
+     * The array should only contain elements of types that are subclasses of java.lang.Number.
+     * @param array The array whose elements are to be summed.
+     * @param <T> The type of elements in the array, which should be a subclass of Number.
+     * @return The sum of all elements in the array as a double.
+     */
+    public static <T extends Number> double sum(T[] array) {
+        double total = 0;
+        for (T element : array) {
+            total += element.doubleValue();
+        }
+        return total;
+    }
 }
--- Main.java
+++ Main.java
@@ -1,8 +1,42 @@
 /**
  * A test class to demonstrate the functionality of the GenericUtils class.
+ * @author Maksym Kovacevic
+ * @version 12-01-2025
  */
 public class Main {
     public static void main(String[] args){
+        // Test the reverse method
+        Integer[] numberArray = {1, 2, 3, 4, 5};
+        System.out.println("Original number array: ");
+        printArray(numberArray);
+        GenericUtils.reverse(numberArray);
+        System.out.println("Reversed number array: ");
+        printArray(numberArray);
+        String[] stringArray = {"Hello", "World", "Java", "Generics"};
+        System.out.println("Original string array: ");
+        printArray(stringArray);
+        GenericUtils.reverse(stringArray);
+        System.out.println("Reversed string array: ");
+        printArray(stringArray);
 
+        // Test the sum method
+        Integer[] intArray = {1, 2, 3, 4, 5};
+        printArray(intArray);
+        System.out.println("Sum of integer array: " + GenericUtils.sum(intArray));
+        Double[] doubleArray = {1.1, 2.2, 3.3, 4.4, 5.5};
+        printArray(doubleArray);
+        System.out.println("Sum of double array: " + GenericUtils.sum(doubleArray));
+    }
+
+    /**
+     * A helper method to print the elements of an array.
+     * @param array The array whose elements are to be printed.
+     * @param <T> The type of elements in the array.
+     */
+    private static <T> void printArray(T[] array) {
+        for (T element : array) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
     }
 }
