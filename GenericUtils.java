919c429a6529aa5c9b6d2f6758ed39df17c5017e
mkovacevic10 <max.billroth@gmail.com>
2025-02-12 23:35:02 +0100

    Create Main and GenericUtils Classes

---
+++ GenericUtils.java
@@ -0,0 +1,6 @@
+/**
+ * A utility class that provides generic methods for arrays.
+ */
+public class GenericUtils {
+
+}
---
+++ Main.java
@@ -0,0 +1,8 @@
+/**
+ * A test class to demonstrate the functionality of the GenericUtils class.
+ */
+public class Main {
+    public static void main(String[] args){
+
+    }
+}
